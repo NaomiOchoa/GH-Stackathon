4022b4c8e0dc12b92b57f56a2cffc72733052cf6
Naomi Ochoa <naomi.m.ochoa@gmail.com>
2020-09-01 10:03:16 -0400

    refactor login page styling

--- src/components/LoginSignupForm.js
+++ src/components/LoginSignupForm.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  Paper,
-  Button,
-  TextField,
-  Link,
-  Typography,
-  Container,
-} from "@material-ui/core";
+import { Button, Form, Grid, Header, Segment } from "semantic-ui-react";
 import Blinky from "./Blinky";
 
 import { signInWithGoogle, auth, createUserProfileDoc } from "../firebase";
@@ -54,124 +47,133 @@ export default class LoginSignupForm extends React.Component {
     const { displayName, email, password, form } = this.state;
     return form === "login" ? (
       <React.Fragment>
-        <Container component="main" maxWidth="sm">
-          <Paper>
-            <Typography component="h1" varient="h5">
-              Sign In
-            </Typography>
-            <form onSubmit={this.handleUserPasswordLogin}>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                value={email}
-                onChange={this.handleChange}
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="password"
-                label="Password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                value={password}
-                onChange={this.handleChange}
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Sign In
-              </Button>
-            </form>
-            <Button
-              type="button"
-              fullWidth
-              variant="contained"
-              color="primary"
-              onClick={signInWithGoogle}
-            >
-              Sign In With Google
-            </Button>
-            <Link
-              href="#"
-              variant="body2"
-              onClick={() => this.setState({ form: "signup" })}
-            >
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Paper>
-        </Container>
+        <Grid
+          textAlign="center"
+          style={{ height: "70vh" }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 700 }}>
+            <Header as="h1" className="site-title">
+              Whatcha Doin?
+            </Header>
+            <Segment>
+              <Grid textAlign="center">
+                <Grid.Row>
+                  <Header as="h2">Sign In</Header>
+                </Grid.Row>
+                <Grid.Row>
+                  <Form onSubmit={this.handleUserPasswordLogin}>
+                    <Form.Field
+                      required
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      control="input"
+                      autoFocus
+                      value={email}
+                      onChange={this.handleChange}
+                    />
+                    <Form.Field
+                      required
+                      id="password"
+                      label="Password"
+                      name="password"
+                      control="input"
+                      type="password"
+                      value={password}
+                      onChange={this.handleChange}
+                    />
+                    <Button type="submit" fullWidth>
+                      Sign In
+                    </Button>
+                  </Form>
+                </Grid.Row>
+                <Grid.Row>
+                  <Button
+                    type="button"
+                    color="primary"
+                    onClick={signInWithGoogle}
+                  >
+                    Sign In With Google
+                  </Button>
+                </Grid.Row>
+                <Grid.Row>
+                  <a href="#" onClick={() => this.setState({ form: "signup" })}>
+                    {"Don't have an account? Sign Up"}
+                  </a>
+                </Grid.Row>
+              </Grid>
+            </Segment>
+          </Grid.Column>
+        </Grid>
         <Blinky />
       </React.Fragment>
     ) : (
-      <Container component="main" maxWidth="sm">
-        <Paper>
-          <Typography component="h1" varient="h5">
-            Sign Up
-          </Typography>
-          <form onSubmit={this.handleSubmitNewUserForm}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="displayName"
-              label="Your Display Name"
-              name="displayName"
-              autoFocus
-              value={displayName}
-              onChange={this.handleChange}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              label="Password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              value={password}
-              onChange={this.handleChange}
-            />
-            <Button type="submit" fullWidth variant="contained" color="primary">
-              Sign Up
-            </Button>
-          </form>
-          <Link
-            href="#"
-            variant="body2"
-            onClick={() => this.setState({ form: "login" })}
-          >
-            {"Already have an account? Sign In"}
-          </Link>
-        </Paper>
-      </Container>
+      <Grid
+        textAlign="center"
+        style={{ height: "70vh" }}
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: 700 }}>
+          <Header as="h1" className="site-title">
+            Whatcha Doin?
+          </Header>
+
+          <Segment>
+            <Grid textAlign="center">
+              <Grid.Row>
+                <Header as="h2">Sign Up</Header>
+              </Grid.Row>
+              <Grid.Row>
+                <Form onSubmit={this.handleSubmitNewUserForm}>
+                  <Form.Field
+                    required
+                    id="displayName"
+                    label="Your Display Name"
+                    name="displayName"
+                    control="input"
+                    autoFocus
+                    value={displayName}
+                    onChange={this.handleChange}
+                  />
+                  <Form.Field
+                    required
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    control="input"
+                    value={email}
+                    onChange={this.handleChange}
+                  />
+                  <Form.Field
+                    required
+                    id="password"
+                    label="Password"
+                    name="password"
+                    control="input"
+                    type="password"
+                    value={password}
+                    onChange={this.handleChange}
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                  >
+                    Sign Up
+                  </Button>
+                </Form>
+              </Grid.Row>
+              <Grid.Row>
+                <a href="#" onClick={() => this.setState({ form: "login" })}>
+                  {"Already have an account? Sign In"}
+                </a>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
